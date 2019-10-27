d7e51d5a88fe6ac1fd6a27e75f922a0c5150878d
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-10-27 14:01:59 +0100

    first commit

---
+++ home/script.js
@@ -0,0 +1,13 @@
+var nLetter = document.querySelector(".n");
+
+var menuButtons = document.querySelectorAll('#menuBars button');
+
+function hoverOverButtons (button) {
+    
+}
+
+menuButtons.forEach(function (button) {
+    button.onclick=hoverOverButtons
+    }
+)
+
